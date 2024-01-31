4212b087d20164e40a5e460209d7e9c584e8b778
Valter Junior <valter.junior@asapcred.com.br>
2024-01-31 17:12:12 -0300

    Update project
    Update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:my_project_gen/firebase_crashlytics.dart';
 import 'app_widget.dart';
-void main() {
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  //FirebaseCoreCrashlytics firebaseCoreCrashlytics = FirebaseCoreCrashlytics();
+  //await firebaseCoreCrashlytics.config();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
